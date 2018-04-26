a1014404e8ff603a6b60fc243cff936c98c3d497
k-kuwahara <zensin0082@gmail.com>
2018-04-26 23:58:12 +0900

    fixed: count about bash history

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,9 @@
 var exec = require('child_process').exec;
 
 var number = process.argv[2] || 10;
-var cmd = 'history | awk \'{print $2}\' | sort | uniq -c | sort -r | head -n ' + number;
+var cmd = 'cat ~/.bash_history | sort | uniq -c | sort -r | head -n ' + number;
 
-exec('history | head -n ' + number, { maxBuffer: 4096 * 4096 }, function (err, stdout, stderr) {
+exec(cmd, function (err, stdout, stderr) {
    if (err) {
       console.error('exec error: ' + err);
       return;
--- src/index.es6
+++ src/index.es6
@@ -2,9 +2,9 @@
 const exec = require('child_process').exec
 
 const number = process.argv[2] || 10
-const cmd = `history | awk '{print $2}' | sort | uniq -c | sort -r | head -n ${number}`
+const cmd = `cat ~/.bash_history | sort | uniq -c | sort -r | head -n ${number}`
 
-exec(`history | head -n ${number}`, {maxBuffer: 4096*4096}, (err, stdout, stderr) => {
+exec(cmd, (err, stdout, stderr) => {
    if (err) {
       console.error(`exec error: ${err}`)
       return
