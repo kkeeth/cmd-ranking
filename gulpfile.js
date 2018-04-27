5d9288eeb7025d9ddeeaa1cc029acfd1abd4a921
k-kuwahara <zensin0082@gmail.com>
2018-04-27 16:14:22 +0900

    fixed: output file name

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 const gulp    = require("gulp"),
       babel   = require("gulp-babel"),
-      plumber = require('gulp-plumber')
+      plumber = require('gulp-plumber'),
+      rename  = require('gulp-rename')
 
 gulp.task('babel', () => {
-  gulp.src('./src/*.es6')
-    .pipe(plumber({
-      errorHandler: (err) => {
-        console.log(err)
-      }
-    }))
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('./bin'))
+   gulp.src('./src/*.es6')
+      .pipe(plumber({
+         errorHandler: (err) => {
+            console.log(err)
+         }
+      }))
+      .pipe(babel({
+         presets: ['es2015']
+      }))
+      .pipe(rename({
+         extname: ''
+      }))
+      .pipe(gulp.dest('./bin'))
 });
 
 gulp.task('watch', () => {
-  gulp.watch('./src/*.es6', ['babel'])
+   gulp.watch('./src/*.es6', ['babel'])
 });
 
 gulp.task('default', ['babel', 'watch'])
